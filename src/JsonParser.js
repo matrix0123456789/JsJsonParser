f90f9616722067c56db7ec4c14b241a8794074d1
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2024-03-30 13:37:43 +0100

    working parser

--- src/JsonParser.js
+++ src/JsonParser.js
@@ -5,48 +5,29 @@ export class JsonParser {
     }
 
     static parse(text) {
-        const parser = new JsonParser(text);
-        return parser.parseAll();
-    }
-
-    parseAll() {
-        const ret = this.parseItem();
-        if (this.position < this.text.length)
-            this.throw();
-        return ret;
+        return (new JsonParser(text)).parseAll();
     }
 
     throw() {
-        throw new Error("Json parsing error on position " + this.position);
+        throw new Error("JSON syntax error on position: " + this.position);
     }
 
-    parseItem() {
+    parseAll() {
         this.skipWhitespaces();
-        if (this.text[this.position] == '[') {
-            return this.parseArray();
-        } else if (this.text[this.position] == '{') {
-            return this.parseObject()
-        } else if (this.text[this.position] == '"') {
-            return this.parseString();
-        } else if (/[0-9\.-]/.test(this.text[this.position])) {
-            return this.parseNumber()
-        } else if (this.text.substring(this.position, this.position + 4) == 'true') {
-            this.position += 4;
-            return true;
-        } else if (this.text.substring(this.position, this.position + 5) == 'false') {
-            this.position += 5;
-            return false;
-        } else if (this.text.substring(this.position, this.position + 4) == 'null') {
-            this.position += 4;
-            return null;
-        } else {
+        if (this.position === this.text.length) {
             this.throw();
         }
+        const ret = this.parseItem();
+        this.skipWhitespaces();
+        if (this.position !== this.text.length) {
+            this.throw();
+        }
+        return ret;
     }
 
     skipWhitespaces() {
         while (this.position < this.text.length) {
-            if (/\s/.test(this.text[this.position])) {
+            if (/\s/.test(this.char())) {
                 this.position++;
             } else {
                 return;
@@ -54,77 +35,39 @@ export class JsonParser {
         }
     }
 
-    parseString() {
+    char() {
+        return this.text[this.position];
+    }
+
+    parseItem() {
         this.skipWhitespaces();
-        if (this.text[this.position] != '"') {
+        if (this.char() == '{') {
+            return this.parseObject();
+        } else if (this.char() == '[') {
+            return this.parseArray()
+        } else if (this.char() == '"') {
+            return this.parseString();
+        } else if (/[-0-9]/.test(this.char())) {
+            return this.parseNumber();
+        } else if (this.text.substring(this.position, this.position + 4) == 'true') {
+            this.position += 4;
+            return true;
+        } else if (this.text.substring(this.position, this.position + 4) == 'null') {
+            this.position += 4;
+            return null;
+        } else if (this.text.substring(this.position, this.position + 5) == 'false') {
+            this.position += 5;
+            return false;
+        }else{
             this.throw();
         }
-        this.position++;
-        let ret = "";
-        while (this.position < this.text.length) {
-            if (this.text[this.position] == '"') {
-                this.position++;
-                return ret;
-            } else if (this.text[this.position] == '\\') {
-                this.position++;
-                if (this.text[this.position] == '\\') {
-                    this.position++;
-                    ret += '\\';
-                } else if (this.text[this.position] == '/') {
-                    this.position++;
-                    ret += '/';
-                } else if (this.text[this.position] == 'r') {
-                    this.position++;
-                    ret += '\r'
-                } else if (this.text[this.position] == 'n') {
-                    this.position++;
-                    ret += '\n'
-                } else if (this.text[this.position] == 't') {
-                    this.position++;
-                    ret += '\t'
-                } else if (this.text[this.position] == 'b') {
-                    this.position++;
-                    ret += '\b';
-                } else if (this.text[this.position] == 'f') {
-                    this.position++;
-                    ret += '\f';
-                } else if (this.text[this.position] == '"') {
-                    this.position++;
-                    ret += '"';
-                } else if (this.text[this.position] == 'x') {
-                    this.position++;
-                    const charCode = parseInt('0x' + this.text.substring(this.position, this.position + 2))
-                    this.position += 2;
-                    ret += String.fromCharCode(charCode);
-
-                } else if (this.text[this.position] == 'u') {
-                    this.position++;
-                    const charCode = parseInt('0x' + this.text.substring(this.position, this.position + 4))
-                    this.position += 4;
-                    ret += String.fromCharCode(charCode);
-
-                } else {
-                    this.throw();
-                }
-            } else {
-                ret += this.text[this.position];
-                this.position++;
-            }
-        }
-        this.throw();
     }
 
     parseNumber() {
         let ret = "";
-        if (/[-0-9]/.test(this.text[this.position])) {
-            ret += this.text[this.position]
-            this.position++;
-        } else {
-            this.throw();
-        }
         while (this.position < this.text.length) {
-            if (/[-+0-9e\.]/.test(this.text[this.position])) {
-                ret += this.text[this.position];
+            if (/[0-9e\.+-]/.test(this.char())) {
+                ret += this.char();
                 this.position++;
             } else {
                 break;
@@ -132,78 +75,138 @@ export class JsonParser {
         }
         if (/^-?0+[0-9]/.test(ret)) {
             this.throw();
+        } else if (/^\+/.test(ret)) {
+            this.throw();
         }
         return parseFloat(ret);
     }
 
-    parseArray() {
-        const ret = [];
+    parseString() {
+        let ret = "";
         this.skipWhitespaces();
-        if (this.text[this.position] != '[')
+        if (this.char() != '"') {
             this.throw();
-        this.position++;
-        this.skipWhitespaces();
-        if (this.text[this.position] == ']') {
-            this.position++;
-            return ret;
-        } else {
-            ret.push(this.parseItem());
         }
+        this.position++;
         while (this.position < this.text.length) {
-            this.skipWhitespaces();
-            if (this.text[this.position] == ']') {
+            if (this.char() == '"') {
                 this.position++;
                 return ret;
-            } else if (this.text[this.position] == ',') {
+            } else if (this.char() == '\\') {
+                this.position++;
+                if (this.char() == '\\') {
+                    this.position++;
+                    ret += '\\';
+                } else if (this.char() == '/') {
+                    this.position++;
+                    ret += '/';
+                } else if (this.char() == 'n') {
+                    this.position++;
+                    ret += '\n';
+                } else if (this.char() == 'r') {
+                    this.position++;
+                    ret += '\r';
+                } else if (this.char() == 't') {
+                    this.position++;
+                    ret += '\t';
+                } else if (this.char() == 'b') {
+                    this.position++;
+                    ret += '\b';
+                } else if (this.char() == 'f') {
+                    this.position++;
+                    ret += '\f';
+                } else if (this.char() == '"') {
+                    this.position++;
+                    ret += '"';
+                } else if (this.char() == 'x') {
+                    this.position++;
+                    const charCode = parseInt(this.text.substring(this.position, this.position + 2), 16)
+                    this.position += 2;
+                    ret += String.fromCharCode(charCode);
+                } else if (this.char() == 'u') {
+                    this.position++;
+                    const charCode = parseInt(this.text.substring(this.position, this.position + 4), 16)
+                    this.position += 4;
+                    ret += String.fromCharCode(charCode);
+                } else {
+                    this.throw();
+                }
+            } else {
+                ret += this.char();
+                this.position++;
+            }
+        }
+
+        this.throw()
+    }
+
+    parseArray() {
+        let ret = [];
+        this.skipWhitespaces();
+        if (this.char() != '[') {
+            this.throw();
+        }
+        this.position++;
+        this.skipWhitespaces();
+        if (this.char() == ']') {
+            this.position++;
+            return ret;
+        }
+        ret.push(this.parseItem())
+        while (this.position < this.text.length) {
+            this.skipWhitespaces();
+            if (this.char() == ']') {
+                this.position++;
+                return ret;
+            } else if (this.char() == ',') {
                 this.position++;
                 this.skipWhitespaces();
-                ret.push(this.parseItem());
+                ret.push(this.parseItem())
             } else {
                 this.throw();
             }
         }
+        this.throw();
     }
 
     parseObject() {
-        const ret = {};
+        let ret = {};
         this.skipWhitespaces();
-        if (this.text[this.position] != '{')
+        if (this.char() != '{') {
             this.throw();
+        }
         this.position++;
         this.skipWhitespaces();
-        if (this.text[this.position] == '}') {
+        if (this.char() == '}') {
             this.position++;
             return ret;
-        } else {
-            const key = this.parseString();
-            this.skipWhitespaces();
-            if (this.text[this.position] != ':')
-                this.throw();
-            this.position++;
-            this.skipWhitespaces()
-            const value = this.parseItem();
-            ret[key] = value;
         }
+        this.readKeyValue(ret);
         while (this.position < this.text.length) {
             this.skipWhitespaces();
-            if (this.text[this.position] == '}') {
+            if (this.char() == '}') {
                 this.position++;
                 return ret;
-            } else if (this.text[this.position] == ',') {
+            } else if (this.char() == ',') {
                 this.position++;
                 this.skipWhitespaces();
-
-                const key = this.parseString();
-                this.skipWhitespaces();
-                if (this.text[this.position] != ':')
-                    this.throw();
-                this.position++;
-                this.skipWhitespaces()
-                const value = this.parseItem();
-                ret[key] = value;
+                this.readKeyValue(ret);
             } else {
                 this.throw();
             }
         }
+        this.throw();
+    }
+
+    readKeyValue(obj) {
+        this.skipWhitespaces();
+        const key = this.parseString();
+        this.skipWhitespaces();
+        if (this.char() != ':')
+            this.throw();
+        this.position++;
+        this.skipWhitespaces();
+        const value = this.parseItem();
+        obj[key] = value;
     }
 }
